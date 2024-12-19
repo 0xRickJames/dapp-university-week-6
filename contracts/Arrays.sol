2b56554dc419077d57cecc39f88510ae573c1ad9
0xRickJames <0xrickjames@gmail.com>
2024-12-19 13:27:06 -0500

    finished arrays

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -1,31 +1,26 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 // Array initalization
 // Homework: try more data types!
 contract Arrays1 {
     uint[] public array1 = [1, 2, 3];
-    uint[] public array2; // this initializes an array of uints
-    uint[10] public array3; // this initializes a fixed size array of 10 uints, default to 0
-    string[] public array4  = ["apple", "banana", "carrot"];
-    string[] public array5; // this initializes an array of strings
-    string[10] public array6; // this initializes a fixed size array of 10 uints, default to ""
+    uint[] public array2;
+    uint[10] public array3;
+    string[] public array4 = ['apple', 'banana', 'carrot'];
+    string[] public array5;
+    string[10] public array6;
 }
 
-
-// Array functions
-// Homework: try manipulating more arrays & data types
 contract Arrays2 {
     uint[] public array;
 
-    // Get value at a given index
     function get(uint i) public view returns (uint) {
         return array[i];
     }
 
-    // Return the whole array
     function getArray() public view returns (uint[] memory) {
         return array;
     }
@@ -34,17 +29,14 @@ contract Arrays2 {
         return array.length;
     }
 
-    // Adds new value at the end of the array
-    function push(uint i) public {
-        array.push(i);
+    function push(uint _value) public {
+        array.push(_value);
     }
 
-    // Removes last item from array
     function pop() public {
         array.pop();
     }
 
-    // Remove item at given index
     function remove(uint index) public {
         delete array[index];
     }

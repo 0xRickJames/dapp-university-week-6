45e9d839d576558563583aaa4bbed9a7c9a9b189
0xRickJames <0xrickjames@gmail.com>
2024-12-21 10:10:08 -0500

    finished up to mappings

--- contracts/Mappings.sol
+++ contracts/Mappings.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 // Mapping examples (key value pairs)
 contract Mappings1 {
-    mapping(uint => string) public names; // Mapping of string like a databse
-    mapping(uint => address) public addresses; // Mapping of string like a databse
-    mapping(address => uint) public balances; // Mapping of string like a databse
-    mapping(address => bool) public hasVoted; // Tracks whether a user has voted
+    mapping(uint => string) public names;
+    mapping(uint => address) public addresses;
+    mapping(address => uint) public balances;
+    mapping(address => bool) public hasVoted;
 
-    // Add values to the mapping in the constructor
     constructor() {
-        names[1] = "Adam";
-        names[2] = "Ben";
+        names[1] = 'Adam';
+        names[2] = 'Ben';
 
         addresses[1] = 0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB;
         addresses[2] = 0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c;
@@ -24,42 +23,32 @@ contract Mappings1 {
         hasVoted[0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB] = true;
         hasVoted[0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c] = true;
     }
-
 }
 
 contract Mappings2 {
-
-    // Mapping of structs
     struct Book {
-        string author;
         string title;
+        string author;
     }
 
     mapping(uint => Book) public books;
 
-    // Nested mapping
-    // Track token balnces for a given account
-    // E.G. Dai: 0x6B175474E89094C44Da98b954EedeAC495271d0F
-    // E.G. WETH: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     mapping(address => mapping(address => uint)) public balances;
 
     constructor() {
-        books[1] = Book("A Tale of Two Cities", "Charles Dickens");
-        books[2] = Book("Les Miserables", "Victor Hugo");
+        books[1] = Book('A Tale of Two Cities', 'Charles Dickens');
+        books[2] = Book('Les Miserables', 'Victor Hugo');
 
         address user1 = 0x3EcEf08D0e2DaD803847E052249bb4F8bFf2D5bB;
         address user2 = 0xe5c430b2Dd2150a20f25C7fEde9981f767A48A3c;
         address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
         address weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-        // User 1 dai balance
         balances[user1][dai] = 1 ether;
         balances[user2][weth] = 2 ether;
     }
-
 }
 
-// Getting and setting values
 contract Mappings3 {
     mapping(uint => string) public myMapping;
 
@@ -72,13 +61,10 @@ contract Mappings3 {
     }
 
     function remove(uint _id) public {
-        // Resets the value to the default value.
         delete myMapping[_id];
     }
 }
 
-
-// Getting and setting nested values
 contract Mappings4 {
     mapping(address => mapping(uint => bool)) public myMapping;
 
@@ -87,11 +73,10 @@ contract Mappings4 {
     }
 
     function set(address _user, uint _id, bool _value) public {
-       myMapping[_user][_id] = _value;
+        myMapping[_user][_id] = _value;
     }
 
     function remove(address _user, uint _id) public {
-        // Resets the value to the default value.
         delete myMapping[_user][_id];
     }
 }
